5e080414680faf4fc32c0e54d38baa6c9c7817ef
Can Sahin <cann2005@gmail.com>
2023-09-09 21:54:41 +0200

    improved validation script

--- scripts/validateJsonFiles.js
+++ scripts/validateJsonFiles.js
@@ -17,6 +17,10 @@ const groupsGeojson = readJsonFile("../communities/groups/groups.geojson");
 const groupsGeojsonSchema = readJsonFile(
   "./jsonSchemas/groupsGeojson.schema.json"
 );
+const geojsonIds = groupsGeojson.features.map(
+  (feature) => feature.properties.id
+);
+const groupsIds = groupsJson.map((group) => group.id);
 
 const validateJsonSchemas = () => {
   const validate = (json, schema, name) => {
@@ -33,9 +37,7 @@ const validateJsonSchemas = () => {
 };
 
 const validateGroupsMatchingIds = () => {
-  const geojsonIds = groupsGeojson.features.map((feature) => feature.properties.id);
-  const groupsIds = groupsJson.map((group) => group.id);
-  const missingIds = geojsonIds.filter((id) => !groupsIds.includes(id));
+  let missingIds = geojsonIds.filter((id) => !groupsIds.includes(id));
   if (missingIds.length > 0) {
     console.error(
       `The following group ids are missing in groups.json: ${missingIds.join(
@@ -44,7 +46,35 @@ const validateGroupsMatchingIds = () => {
     );
     process.exit(1);
   }
+  missingIds = groupsIds.filter((id) => !geojsonIds.includes(id));
+  if (missingIds.length > 0) {
+    console.error(
+      `The following group ids are missing in groups.geojson: ${missingIds.join(
+        ", "
+      )}`
+    );
+    process.exit(1);
+  }
+};
+
+const validateUniqueIds = () => {
+  const checkDuplicates = (array) => {
+    const duplicates = array.filter(
+      (item, index) => array.indexOf(item) != index
+    );
+    if (duplicates.length > 0) {
+      console.error(
+        `The following ids are duplicated: ${duplicates.join(
+          ", "
+        )}`
+      );
+      process.exit(1);
+    }
+  };
+  checkDuplicates(groupsIds);
+  checkDuplicates(geojsonIds);
 };
 
 validateJsonSchemas();
 validateGroupsMatchingIds();
+validateUniqueIds();
